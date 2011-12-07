eba39408657b770924cf695778457ee0a78d7cc9
Vlad Dumitriu <vladut.mail@gmail.com>
2011-12-07 08:18:34 +0200

    101 poly fit

---
+++ 101/oneoone.m
@@ -0,0 +1,50 @@
+function [ans] = oneoone()
+two()
+val = zeros(1,10);
+for i = 1:10
+  val(1,i) = f([1,-1,1,-1,1,-1,1,-1,1,-1,1],i);
+end
+ssum = 0;
+for i = 1:10
+  x = [];
+  y = val(1,1:i);
+  for j = 1:i
+    x = [x, j];
+  end
+  bop = f(polyfit(x, y, i-1), i+1)
+  ssum = ssum + bop;
+  sprintf("%d", bop)
+end
+ sprintf("%d", ssum)
+ans = val;
+end
+function [ans] = two()
+val = zeros(1,5);
+for i = 1:5
+  val(1,i) = f([1, 0, 0, 0],i);
+end
+ssum = 0;
+for i = 1:3
+  x = [];
+  y = val(1,1:i);
+  for j = 1:i
+    x = [x, j];
+  end
+  bop = f(polyfit(x, y, i-1), i+1)
+  ssum = ssum + bop;
+  sprintf("%d", bop)
+end
+ sprintf("%d", ssum)
+ans = val;
+end
+function [ans] = f(g, n)
+  m = size(g,2);
+  ans = 0;
+  i = 1;
+  while m > 0
+    r = n**(m-1);
+    ans = ans + g(1,i)*r;
+    m = m - 1;
+    i = i + 1;
+  end
+end
